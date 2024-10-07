0582d713c9f788332656015228a9e750ee37baa6
alexwox <alexander.woxstrom@gmail.com>
2024-10-07 20:45:19 +0200

    tailwind issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Providers from "../providers";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Providers>
         <main>
         <SignedOut>
           <SignInButton />
@@ -13,8 +11,6 @@ export default function Home() {
           <UserButton />
         </SignedIn>
         </main>
-      </Providers>
-
     </div>
   );
 }
