35970e7895a0f2958327289f84c9d2ba23654d94
alexwox <alexander.woxstrom@gmail.com>
2024-10-15 22:51:51 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "https://upbeat-narwhal-999.convex.cloud",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
