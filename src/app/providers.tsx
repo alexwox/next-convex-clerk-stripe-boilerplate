11b1199c20ddb50116e26e04a1c61c4581165936
alexwox <alexander.woxstrom@gmail.com>
2024-10-17 12:43:30 +0200

    redirect after login

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import {
-    ClerkProvider,
-    useAuth
+  ClerkProvider,
+  useAuth
 } from '@clerk/nextjs'
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { PropsWithChildren } from 'react';
@@ -12,21 +12,24 @@ import { ThemeProvider } from './theme-provider';
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 export function Providers({
-    children,
+  children,
 }: PropsWithChildren
 ) {
-    return (
-      <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
-        <ConvexProviderWithClerk useAuth={useAuth} client={convex} >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-          {children}
-          </ThemeProvider>
-        </ConvexProviderWithClerk>
-      </ClerkProvider>
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      signInFallbackRedirectUrl="/"
+      >
+  <ConvexProviderWithClerk useAuth={useAuth} client={convex} >
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  </ConvexProviderWithClerk>
+      </ClerkProvider >
     )
 }
